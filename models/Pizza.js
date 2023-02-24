096b94eb5bba7632a9575dc0e17ccde92e80be1b
Alissa Cohan <alissacohan18@gmail.com>
2023-02-24 10:37:28 -0500

    added CRUD for comments

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,27 +1,53 @@
 //importing Schema constructor and model function
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
 
-const PizzaSchema = new Schema({
+const PizzaSchema = new Schema(
+  {
     pizzaName: {
-      type: String
+      type: String,
     },
     createdBy: {
-      type: String
+      type: String,
     },
     createdAt: {
       type: Date,
-      default: Date.now
+      default: Date.now,
+      //use mongoose "getters" to transform date into specified format when displayed
+      get: (createdAtVal) => dateFormat(createdAtVal),
     },
     size: {
       type: String,
-      default: 'Large'
+      default: "Large",
     },
-    // [] indicate type is array
-    toppings: []
-  });
+    toppings: [],
+    comments: [
+      {
+        type: Schema.Types.ObjectId,
+        //The ref property tells the Pizza model which documents to search
+        //to find the right comments.
+        ref: "Comment",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      //tells mongoose model to use any getter function specified
+      getters: true
+    },
+    //set to false bc it's a virtual that Mongoose returns & we don’t need it
+    id: false,
+  }
+);
 
-  // create the Pizza model using the PizzaSchema
-const Pizza = model('Pizza', PizzaSchema);
+// get total count of comments and replies on retrieval
+PizzaSchema.virtual("commentCount").get(function () {
+  return this.comments.length;
+});
+
+// create the Pizza model using the PizzaSchema
+const Pizza = model("Pizza", PizzaSchema);
 
 // export the Pizza model
 module.exports = Pizza;

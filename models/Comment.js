15eb3d589b3071393f3b2a2f5616bda579c69a3f
Alissa Cohan <alissacohan18@gmail.com>
2023-02-24 12:31:45 -0500

    added CRUD for replies

--- models/Comment.js
+++ models/Comment.js
@@ -1,18 +1,66 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, Types } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
 
-const CommentSchema = new Schema({
-  writtenBy: {
-    type: String
+const ReplySchema = new Schema(
+  {
+    // set custom id to avoid confusion with parent comment _id
+    //replyID requires using mongoose "Types" so added to the const {} at the top
+    replyId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    replyBody: {
+      type: String,
+    },
+    writtenBy: {
+      type: String,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
   },
-  commentBody: {
-    type: String
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now
+  {
+    toJSON: {
+      getters: true,
+    },
   }
+);
+
+const CommentSchema = new Schema(
+  {
+    writtenBy: {
+      type: String,
+    },
+    commentBody: {
+      type: String,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
+    // use ReplySchema to validate data for a reply
+    //associates replies with comments; the field replies populates an array of
+    //data that adheres to the ReplySchema definition
+    replies: [ReplySchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+CommentSchema.virtual("replyCount").get(function () {
+  return this.replies.length;
 });
 
-const Comment = model('Comment', CommentSchema);
+// create the Comment model using the CommentSchema
+const Comment = model("Comment", CommentSchema);
 
+// export the Comment model
 module.exports = Comment;
--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -2,13 +2,24 @@ const router = require("express").Router();
 const {
   addComment,
   removeComment,
+  addReply,
+  removeReply,
 } = require("../../controllers/comment-controller");
 
 // /api/comments/<pizzaId>
-router.route('/:pizzaId').post(addComment);
+router.route("/:pizzaId").post(addComment);
 
 // /api/comments/<pizzaId>/<commentId>
-router.route('/:pizzaId/:commentId').delete(removeComment);
+router
+  .route("/:pizzaId/:commentId")
+  //use PUT vs POST b/c technically we're not creating a new reply resource;
+  // we're just updating the existing comment resource
+  .put(addReply)
+  .delete(removeComment);
 
+  //modeling routes in a RESTful manner: as a best practice, include the ids of the
+  //parent resources in the endpoint. Suggests "Go to this pizza, then look at this
+  // particular comment, then delete this one reply."
+router.route("/:pizzaId/:commentId/:replyId").delete(removeReply);
 
 module.exports = router;
